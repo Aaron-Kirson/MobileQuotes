e71e626aea520b6a2ba98c8bd40f1a9bbdb0f535
Aaron_kirson <36044246+badaaron@users.noreply.github.com>
2019-09-13 22:31:57 +0100

    Add project files.

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+    /// <summary>
+    /// Defines the <see cref="MainPage" />
+    /// </summary>
+    [DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+			InitializeComponent();
+
+            slider.Value = 0.5;
+       
+        }
+    }
+}
